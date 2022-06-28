d452ab326911498fab2a586004e0834feaaa10b6
sharipov <ruslan.b.sharipov@gmail.com>
2022-06-28 10:00:49 +0300

    shared flow flatmap

--- app/src/main/java/dev/surf/androidsummerschool2022/MainActivity.kt
+++ app/src/main/java/dev/surf/androidsummerschool2022/MainActivity.kt
@@ -3,21 +3,27 @@ package dev.surf.androidsummerschool2022
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.lifecycle.lifecycleScope
 import dev.surf.androidsummerschool2022.api.CatApi
 import dev.surf.androidsummerschool2022.api.CatResponse
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
+    private val sharedFlow = MutableSharedFlow<Unit>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.hello_tv).setOnClickListener {
+            lifecycleScope.launch {
+                sharedFlow.emit(Unit)
+            }
+        }
 
         val catApi = CatApi.create()
 
@@ -26,6 +32,16 @@ class MainActivity : AppCompatActivity() {
                 async { catApi.getRandomCat("cute") }.await(),
                 async { catApi.getRandomCat("funny") }.await()
             ).forEach {
+                Log.d("onCreate", "$it")
+            }
+        }
+
+        lifecycleScope.launch {
+            sharedFlow.flatMapLatest {
+                flow<CatResponse> {
+                    catApi.getRandomCat("funny")
+                }
+            }.collect {
                 Log.d("onCreate", "$it")
             }
         }
